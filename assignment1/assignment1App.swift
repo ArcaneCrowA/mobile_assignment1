83c9d7212742fb53d832e6f6decc0382983b4007
Adilet <adiletbeishekeyev@gmail.com>
2025-09-19 19:17:05 +0500

    Initial Commit

---
+++ assignment1/assignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  assignment1App.swift
+//  assignment1
+//
+//  Created by Adilet Beishekeyev on 19.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
